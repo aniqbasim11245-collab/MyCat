0388449991ced18f8b7e3cfbfdaca28c74e90b21
aniqbasim11245 <aniqbasim11245@gmail.com>
2026-06-17 17:54:49 +0800

    change html and css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log("MyCAT website loaded");
+
+const reveals = document.querySelectorAll('.reveal');
+const observer = new IntersectionObserver(
+(entries)=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add('show');
+}
+});
+},
+{
+threshold:0.15
+}
+);
+reveals.forEach(item=>{
+observer.observe(item);
+});
